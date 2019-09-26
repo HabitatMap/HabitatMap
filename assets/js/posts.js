e6a66681513750537f2fde8b8225e489be777bce
Malgorzata <malgorzata@lunarlogic.io>
2019-09-26 14:28:10 +0200

    Post list grid toggle styles

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const listToggleButton = document.querySelector(".js--posts-grid-toggle");
+  const listToggleButton = document.querySelector(".js--posts-toggle");
 
   listToggleButton.addEventListener("click", () => {
     const postList = document.querySelector(".post-list");
     postList.classList.toggle("post-list--grid-view");
+    listToggleButton.classList.toggle("post-toggle--list-view");
   });
 });
