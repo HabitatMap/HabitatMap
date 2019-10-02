64b0df1edbfe3e9d8482a5395e33e15da4a9c742
Julia Jacobs <julia@lunarlogic.io>
2019-10-02 14:29:39 +0200

    Fix indentation

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,18 +1,18 @@
-const accItem = document.getElementsByClassName('faq__category');
-const accHD = document.getElementsByClassName('js--faq-heading');
+const accItem = document.getElementsByClassName("faq__category");
+const accHD = document.getElementsByClassName("js--faq-heading");
 
 for (i = 0; i < accHD.length; i++) {
-  accHD[i].addEventListener('click', toggleItem, false);
+  accHD[i].addEventListener("click", toggleItem, false);
 }
 
 function toggleItem() {
   const itemClass = this.parentNode.className;
 
   for (i = 0; i < accItem.length; i++) {
-      accItem[i].className = 'faq__category close';
+    accItem[i].className = "faq__category close";
   }
 
-  if (itemClass == 'faq__category close') {
-      this.parentNode.className = 'faq__category open';
+  if (itemClass == "faq__category close") {
+    this.parentNode.className = "faq__category open";
   }
 }
