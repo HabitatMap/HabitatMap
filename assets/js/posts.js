ea0e8ca9b6aeb923a3731e8de7591151dad5b811
Malgorzata <malgorzata@lunarlogic.io>
2019-10-10 12:16:45 +0200

    Add arc to posts index and placeholder container for thumbnails

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const listToggleButton = document.querySelector(".js--posts-toggle");
+  const postsContainer = document.querySelector(".js--posts-index");
 
   listToggleButton.addEventListener("click", () => {
     const postList = document.querySelector(".post-list");
     postList.classList.toggle("post-list--grid-view");
+    postsContainer.classList.toggle("posts-index--with-arc");
     listToggleButton.classList.toggle("post-toggle--list-view");
   });
 });
