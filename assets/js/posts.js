3e31fba95e293b09ee13c4b2df2f59585c6d14c1
Julia Jacobs <julia@lunarlogic.io>
2019-10-17 18:45:19 +0200

    Make 'grid view' the default for post index

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const listToggleButton = document.querySelector(".js--posts-toggle");
+  const listToggleButton = document.querySelector(".js--post-view-toggle");
   const postsContainer = document.querySelector(".js--posts-index");
 
   listToggleButton.addEventListener("click", () => {
     const postList = document.querySelector(".post-list");
     postList.classList.toggle("post-list--grid-view");
     postsContainer.classList.toggle("posts-index--with-arc");
-    listToggleButton.classList.toggle("post-toggle--list-view");
+    listToggleButton.classList.toggle("post-view-toggle--grid");
   });
 });
