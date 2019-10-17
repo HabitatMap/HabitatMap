8652a36c5149a8a41cf122869c40867f7fa712b7
Julia Jacobs <julia@lunarlogic.io>
2019-10-17 13:39:28 +0200

    Refactor: use dedicate js class, rename vars

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,15 +1,15 @@
-const accItem = document.getElementsByClassName("faq__category");
-const accHD = document.getElementsByClassName("js--faq-heading");
+const allHeadings = document.getElementsByClassName("js--faq-heading");
+const allSections = document.getElementsByClassName("js--faq-section");
 
-for (i = 0; i < accHD.length; i++) {
-  accHD[i].addEventListener("click", toggleItem, false);
+for (i = 0; i < allHeadings.length; i++) {
+  allHeadings[i].addEventListener("click", toggleItem, false);
 }
 
 function toggleItem() {
   const wasItemOpen = this.parentNode.classList.contains("open");
 
-  for (i = 0; i < accItem.length; i++) {
-    accItem[i].classList.remove("open");
+  for (i = 0; i < allSections.length; i++) {
+    allSections[i].classList.remove("open");
   }
 
   if (wasItemOpen !== true) {
