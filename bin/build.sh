f160ff89ac0861ac4484e9e9c97805fcc95e49c6
3v0k4 <riccardo.odone@gmail.com>
2019-12-06 11:59:57 +0100

    basic google analytics

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -x
 
-jekyll build
-
 if [ "$CONTEXT" == "production" ]
 then
+  JEKYLL_ENV=production jekyll build
   ALGOLIA_API_KEY="$ALGOLIA_API_KEY" jekyll algolia
+else
+  jekyll build
 fi
