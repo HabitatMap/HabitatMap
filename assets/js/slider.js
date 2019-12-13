371bcb1c53edb37e49e11d8970b53872c49f22fb
Julia Jacobs <julia@lunarlogic.io>
2019-12-13 16:35:33 +0100

    Fix jumping content by turning off autoHeight option for tiny slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   [].forEach.call(document.querySelectorAll(".js-slider"), function(element) {
     tns({
       container: element,
-      autoHeight: true,
       speed: 400,
       autoplay: true,
       autoplayButtonOutput: false,
