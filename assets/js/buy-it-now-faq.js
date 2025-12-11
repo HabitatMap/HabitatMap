d91486ad9a4807df6d8c11fe5258c456d5458b35
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2025-12-11 21:47:22 +0100

    First part of sections improvemnt

--- assets/js/buy-it-now-faq.js
+++ assets/js/buy-it-now-faq.js
@@ -8,6 +8,7 @@ function toggleFAQ(button) {
     content.classList.add('show');
     icon.style.transform = 'rotate(180deg)';
     button.style.backgroundColor = '#f9fafb';
+    button.style.borderRadius = '1rem 1rem 0 0';
   } else {
     content.classList.remove('show');
     icon.style.transform = 'rotate(0deg)';
